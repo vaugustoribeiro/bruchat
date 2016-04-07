a7bddf41be0434a73e5c6cdc90afcdc69e6f3aa7
vaugustoribeiro <varibeiro@ecad.org.br>
2016-04-07 18:47:34 -0300

    refatoração dos modos de jogo, tendo agora apenas só uma viewmodel para todos eles :D

--- public/javascripts/mesaViewModel.js
+++ public/javascripts/mesaViewModel.js
@@ -4,132 +4,52 @@ function mesaViewModel() {
 
     self.readonly = ko.observable();
 
-    self.numeroCarta = ko.observable();
-    self.cartasSelecionadas = ko.observableArray();
-
-    self.selecionarCarta = function(cartaSelecionada) {
-        self.cartasSelecionadas.push(cartaSelecionada);
-        socket.emit('fc-exibir-carta', cartaSelecionada);
-    };
-
     self.online = ko.observable();
 
-    self.removerCarta = function(cartaSelecionada) {
-        if (self.readonly()) return;
-        self.cartasSelecionadas.splice(self.cartasSelecionadas.indexOf(cartaSelecionada), 1);
-        socket.emit('fc-remover-carta', cartaSelecionada);
+    self.modosDeJogo = [];
+    
+    self.modoDeJogoSelecionado = ko.observable();
+    
+    self.selecionarModoDeJogo = function(modoDeJogo) {
+        socket.emit('fc-selecionar-modo-de-jogo', modoDeJogo.id);
+        modoDeJogo.carregar();
     };
-
-    socket.on('fs-exibir-carta', function(cartaSelecionada) {
-        self.cartasSelecionadas.push(cartaSelecionada);
+    
+    _([
+        ['circuloMagico','O Círculo Mágico (Mandala Cigana)'],
+        ['estrelaCincoPontas','Estrela de Cinco Pontas (Pentagrama)'],
+        ['tresCartas','3 Cartas'],
+        ['mandalaAstrologica','Mandala Astrológica'],
+        ['vinteUmaCartas','21 Cartas'],
+        ['ferradura','Ferradura'],
+        ['magiaCigana','Magia Cigana (Mesa Real)'],
+        ['cruzCelta','Cruz Celta'],
+        ['semana','Semana']
+    ]).forEach(function(value, key) {
+        var modo = new modoDeJogo();
+        modo.id = key++;
+        modo.nome = value[1];
+        modo.view = value[0];
+        self.modosDeJogo.push(modo);
     });
-
-    socket.on('fs-remover-carta', function(cartaSelecionada) {
-        var x = _.find(self.cartasSelecionadas(), function(carta) {
-            return carta.numero === cartaSelecionada.numero;
-        });
-        self.cartasSelecionadas.splice(self.cartasSelecionadas.indexOf(x), 1);
+    
+    socket.on('fs-selecionar-modo-de-jogo', function(id) {
+         var modoDeJogo = _.find(self.modosDeJogo, function(e) {
+            return e.id === id; 
+         });
+         
+         if(modoDeJogo) {
+             modoDeJogo.carregar();
+         }
     });
+}
 
-    self.numeroCarta = ko.observable();
-    self.numeroCartaFoco = ko.observable(false);
-
-    self.adicionarCartaEnter = function(d, e) {
-        e.keyCode === 13 && self.adicionarCarta();
-        return true;
+function modoDeJogo() {
+    var self = this;
+    self.id = 0;
+    self.nome = '';
+    self.view = ''
+    self.carregar = function() {
+        $("#modoDeJogoSelecionado").load("/modosDejogo/" + self.view);
     };
-
-    self.adicionarCarta = function() {
-        var cartaSeleciona = _.find(self.cartas, function(carta) {
-            return carta.numero.toString() === self.numeroCarta();
-        });
-
-        if (cartaSeleciona == null) {
-            vm.modal().exibirMensagem("Não existe carta com este número.")
-        }
-        else {
-            self.selecionarCarta(cartaSeleciona);
-            self.numeroCarta('');
-            self.numeroCartaFoco(true);
-        }
-    }
-
-    self._esconderDescricaoCartas = ko.observable(false);
-    self.esconderDescricaoCartas = ko.computed(function() {
-        return self._esconderDescricaoCartas() && self.readonly();
-    }, self)
-
-    self._esconderDescricaoCartas.subscribe(function(esconderDescricaoCartas) {
-        socket.emit('fc-esconder-descricao-carta', esconderDescricaoCartas);
-    }, self);
-
-    socket.on('fs-esconder-descricao-carta', function(esconderDescricaoCarta) {
-        self._esconderDescricaoCartas(esconderDescricaoCarta);
-    });
-
-    self.modosDeJogo = [
-        {
-            nome: 'O Círculo Mágico (Mandala Cigana)',
-            view: 'circuloMagico',
-            selecionar: function() {
-                     $("#modoDeJogoSelecionado").load('/modosDejogo/circuloMagico');
-            }
-        },
-        {
-            nome: 'Estrela de Cinco Pontas (Pentagrama)',
-            view: 'estrelaCincoPontas',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/estrelaCincoPontas');
-            }
-        },
-        {
-            nome: '3 Cartas',
-            view: 'tresCartas',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/tresCartas');
-            }
-        },
-        {
-            nome: 'Mandala Astrológica',
-            view: 'mandalaAstrologica',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/mandalaAstrologica');
-            }
-        },
-        {
-            nome: '21 Cartas',
-            view: 'vinteUmaCartas',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/vinteUmaCartas');
-            }
-        },
-        {
-            nome: 'Ferradura',
-            view: 'ferradura',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/ferradura');
-            }
-        },
-        {
-            nome: 'Magia Cigana',
-            view: 'magiaCigana',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/magiaCigana');
-            }
-        },
-        {
-            nome: 'Cruz Celta',
-            view: 'cruzCelta',
-            selecionar: function() {
-                 $("#modoDeJogoSelecionado").load('/modosDejogo/cruzCelta');
-            }
-        },
-        {
-            nome: 'Semana',
-            view: 'semana',
-            selecionar: function() {
-                $("#modoDeJogoSelecionado").load('/modosDejogo/semana');
-            }
-        }
-    ];
 }
